4290d3d5b805ba6066de160aa4d900f823ea5dd3
Savera <Level 2>
2017-02-15 18:30:54 -0800

    ice cream ice cream ice cream banana

---
+++ src/BananaSplit.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class BananaSplit {
+public static void main(String[] args) {
+	String banana ="";
+	for (int i = 1; i <=3 ; i++) {
+		
+
+		banana = banana + "ice cream\n";
+	}
+		JOptionPane.showMessageDialog(null, banana + "banana");
+
+	
+}	
+}
