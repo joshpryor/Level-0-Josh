b5ef08ff147830cb558784db55364678641571f4
joshpryor <league@Mac-mini-2.local>
2017-03-01 18:27:10 -0800

    stuff

--- src/EverythigIsAwesome.java
+++ src/EverythigIsAwesome.java
@@ -3,17 +3,32 @@ import javax.swing.JOptionPane;
 public class EverythigIsAwesome {
 public static void main(String[] args) {
 	String awesome= "";
+	String cool= "";
+	String rugby="";
+	String marc= "";
+	String josh= "";
 	while (!awesome.equalsIgnoreCase("stop")) {
 	
 	
 	awesome = JOptionPane.showInputDialog("enter A noun that is awesome");
-	if (awesome.equalsIgnoreCase("brocolli")) {
+
+	
+	if (awesome.equalsIgnoreCase("marc")) {
 		JOptionPane.showMessageDialog(null, awesome+" is not awesome");
 	}
 	else {
 		
-	
-	JOptionPane.showMessageDialog(null, awesome+" is awesome");
-}}
+		
+		JOptionPane.showMessageDialog(null, awesome+" is awesome");
+	}
+	cool = JOptionPane.showInputDialog("favorite food");
+	JOptionPane.showMessageDialog(null, cool + "is awesome");
+	rugby= JOptionPane.showInputDialog("best  friend");
+	JOptionPane.showMessageDialog(null, rugby + "is awesome");
+	marc = JOptionPane.showInputDialog("favorite book?");
+	JOptionPane.showMessageDialog(null, marc + "is awesome");
+	josh = JOptionPane.showInputDialog("best movie?");
+	JOptionPane.showMessageDialog(null, josh + "is awesome");
+	}
 }
 }
