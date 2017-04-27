884834371f5428e7c1e447f69c9775421bd1975e
joshpryor <league@WorthyMini.local>
2017-04-26 18:03:41 -0700

    more stood

--- src/RandomStuff.java
+++ src/RandomStuff.java
@@ -7,12 +7,20 @@ public static void main(String[] args) {
 	Robot neb =new Robot("batman");
 	neb.penDown();
 	neb.setSpeed(999999);
-	neb.move(300);
+	neb.move(100);
 	neb.turn(90);
-	neb.move(300);
+	neb.move(100);
 	neb.turn(90);
-	neb.move(300);
+	neb.move(100);
 	neb.turn(90);
-	neb.move(300);
+	neb.move(200);
+	neb.turn(90);
+	neb.move(200);
+	neb.turn(90);
+	neb.move(200);
+	neb.turn(90);
+	neb.move(200);
+	neb.turn(135);
+	neb.move(230);
 }
 }
